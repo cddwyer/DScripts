e66d3de113ed07069c5cd2507d8b74ab89dbeccd
Christian Dwyer <christian.dwyer@trams.co.uk>
2018-01-30 12:52:29 +0000

    Corrected warning

--- Custom Nagging Notification.sh
+++ Custom Nagging Notification.sh
@@ -34,7 +34,7 @@ todayYY=$(date +%F | cut -d"-" -f1)
 
 /usr/bin/curl -s -o /tmp/logo.png $companyLogoURL
 
-userChoice=$(/Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType hud -lockHUD -title "We need to deliver some software to your computer" -timeout 900 -defaultButton 1 -icon /tmp/logo.png -description "We need to install some important software updates on your computer. The process can take up to 40 minutes. You will receive this notification once a day and you can only defer the update a " -alignDescription left -alignHeading left -button1 "Continue" -button2 "Cancel")
+userChoice=$(/Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType hud -lockHUD -title "We need to deliver some software to your computer" -timeout 900 -defaultButton 1 -icon /tmp/logo.png -description "We need to install some important software updates on your computer. The process can take up to 40 minutes. You will receive this notification once a day and you can only defer up to $deferLimitDay/$deferLimitMonth/$deferLimitYear on which day the update will be installed regardless of your choice." -alignDescription left -alignHeading left -button1 "Continue" -button2 "Cancel")
 
 if [ "$userChoice" == "0" ]; then
    echo "User clicked continue"
